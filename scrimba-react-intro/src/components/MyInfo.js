c180e13268c2d8046916f54d8b714889abf2f09b
Doug Sewell <dougsewell@pop-os.localdomain>
2019-11-20 20:37:38 -0500

    Relocate components for organization.

---
+++ scrimba-react-intro/src/components/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyInfo() {
+    return(
+        <div>
+        <h1>Doug</h1>
+        <p>Web developer, enjoy Disney, and am a tech enthusiast.</p>
+        <p>Places I'd love to visit</p>
+        <ol>
+            <li>Hawaii</li>
+            <li>New Orleans</li>
+            <li>New Zealand</li>
+        </ol>
+    </div>
+    )
+}
+
+export default MyInfo;
--- scrimba-react-intro/src/index.js
+++ scrimba-react-intro/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import MyInfo from './components/MyInfo';
 
-ReactDOM.render(<ul>
-                    <li>Item One</li>
-                    <li>Item Two</li>
-                    <li>Item Three</li>
-                </ul>, document.getElementById('root'));
+
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
