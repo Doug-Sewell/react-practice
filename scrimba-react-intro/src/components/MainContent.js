bb50f69e20155cae81074cf0eaf78a3c50da6356
Doug Sewell <dougsewell@pop-os.localdomain>
2019-12-01 22:42:46 -0500

    Added checked/unchecked todo functionality.

--- scrimba-react-intro/src/components/MainContent.js
+++ scrimba-react-intro/src/components/MainContent.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import todosData from '../data/todoData';
 
-
 class MainContent extends Component {
     constructor() {
         super();
         this.state = {
             todoItems: todosData
         }
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const newState = prevState.todoItems.map(todo => {
+                if(id === todo.id) {
+                    todo.completed= !todo.completed;
+                }
+                return todo;
+            });
+            return newState;
+        });
+    }
+
+
+
     render() {
         const toodosMapped = this.state.todoItems.map(todo => {
-            return (<TodoItem key={todo.id} todo={todo} />)
+            return (<TodoItem handleChange={this.handleChange} key={todo.id} todo={todo} />)
         });
 
         return (
--- scrimba-react-intro/src/components/TodoItem.js
+++ scrimba-react-intro/src/components/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TodoItem = (props) => {
     return (
         <div className="TodoItem">
-            <input type="checkbox" id={props.todo.id} checked={props.todo.completed} />
+            <input type="checkbox" onChange={() => {props.handleChange(props.todo.id)}} id={props.todo.id} checked={props.todo.completed} />
             <label htmlFor={props.todo.id}>{props.todo.text}</label>
         </div>
     )
